9934fcda1a406b2546482dd2ebfc9a456ab53093
Tareq Judeh <tareq.joudeh@gmail.com>
2020-11-09 17:29:04 +0000

    Uploading project files to GitHub.

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
 #Write your code below this row 👇
 
+even_numbers = 0
+
+for num in range(1, 101):
+  if num % 2 == 0:
+    even_numbers += num
+
+print(even_numbers)
+
+# another solution using the third parameter in range()
+
+total = 0
+for num in range(0, 101, 2):
+  total += num
+
+print(total)
